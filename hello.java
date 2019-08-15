404bf099bdfec766a53c939280d7f03ef12531bc
preethi1012 <53898337+preethi1012@users.noreply.github.com>
2019-08-15 16:18:37 +0530

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,21 @@
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class hello
+{
+	public static void main (String[] args)
+	{
+	 int n ;
+	 Scanner s=new Scanner(System.in);
+	 System.out.println("enter number of times");
+	 n=s.nextInt();
+	 for(int i=0;i<=n;i++)
+	 {
+	 	System.out.println("hello");
+	 }
+	 
+	}
+}
